9b2ec48588362aab5ba4869f185cd52dad36d68b
milkamt <mimamatuskova@gmail.com>
2022-09-27 18:32:00 +0200

    .Ã

--- src/test/java/com/springbootforecast/SpringBootForecastApplicationTests.java
+++ src/test/java/com/springbootforecast/SpringBootForecastApplicationTests.java
@@ -1,13 +1,13 @@
-package com.springbootforecast;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringBootForecastApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.springbootforecast;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBootForecastApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
