3c7bb855ff71e0a0774c89aeeb2d01e24ec9675b
milkamt <mimamatuskova@gmail.com>
2022-09-27 19:52:19 +0200

    Edit circleci config / add database

--- src/test/java/com/springbootforecast/service/WeatherApiServiceImplTests.java
+++ src/test/java/com/springbootforecast/service/WeatherApiServiceImplTests.java
@@ -10,30 +10,24 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-
+@SpringBootTest
 public class WeatherApiServiceImplTests {
 
-//    @Autowired
-//    private WeatherApiService weatherApiService;
-//
-//    @Test
-//    public void getWeatherOK() {
-//        Weather weather = weatherApiService.getWeather("Bratislava");
-//
-//        assertThat(weather).isNotNull();
-//    }
-//
-//    @Test
-//    public void getCentralEuropeCapitalsOK() {
-//        List<Weather> weathers = weatherApiService.getCentralEuropeCapitals();
-//
-//        assertEquals(weathers.size(), 5);
-//    }
+    @Autowired
+    private WeatherApiService weatherApiService;
 
     @Test
-    public void exampleTestOK() {
-        assertEquals(5, 2+3);
+    public void getWeatherOK() {
+        Weather weather = weatherApiService.getWeather("Bratislava");
+
+        assertThat(weather).isNotNull();
+    }
+
+    @Test
+    public void getCentralEuropeCapitalsOK() {
+        List<Weather> weathers = weatherApiService.getCentralEuropeCapitals();
+
+        assertEquals(weathers.size(), 5);
     }
 
 }
